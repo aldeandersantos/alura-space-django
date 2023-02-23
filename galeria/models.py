9fa286daf345b75e2909654673f9918b6aea0d07
aldeander-santos@hotmail.com <aldeander-santos@hotmail.com>
2023-02-23 12:07:13 -0300

    :recycle: refactor(crud): Introduzido painel de admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
 class Fotografia(models.Model):
-    nome = models.CharField(max_length=100, null=False, blank=False),
-    legenda = models.CharField(max_length=50, null=False, blank=False),
-    descricao = models.TextField(null=False, blank=False),
-    foto = models.CharField(max_length=100, null=False, blank=False),
-    
-    def __srt__(self):
-        return f"Fotografia [nome=(self.nome)]"
 
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALAXIA", "Galaxia"),
+        ("PLANETA", "Planeta"),
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
+        
 # Create your models here.
